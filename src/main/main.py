8800a81733abea41fa5cb60e72486c18a35ff040
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-05-23 09:54:54 -0300

    feat: create a rest client and send message with twilio rest api

---
+++ src/main/main.py
@@ -0,0 +1,13 @@
+from twilio.rest import Client 
+from config.env import envs
+
+_envs = envs()
+twilio_rest_client = Client(_envs["TWILIO_ACCOUNT_SID"], _envs["TWILIO_AUTH_TOKEN"])
+
+message = twilio_rest_client.messages.create(
+  body = f"Aloy, test from python!",
+  from_=_envs["TWILIO_FROM_NUMBER"],
+  to=f"whatsapp:+5511999999999"
+)
+
+print("Message SID: " + message.sid)
