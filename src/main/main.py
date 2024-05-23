60204f9cd1e8cfafd8c2e4ecdfe62552a80e9b19
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-05-23 09:56:21 -0300

    feat: send media file along with message

--- src/main/main.py
+++ src/main/main.py
@@ -7,7 +7,8 @@ twilio_rest_client = Client(_envs["TWILIO_ACCOUNT_SID"], _envs["TWILIO_AUTH_TOKE
 message = twilio_rest_client.messages.create(
   body = f"Aloy, test from python!",
   from_=_envs["TWILIO_FROM_NUMBER"],
-  to=f"whatsapp:+5511999999999"
+  to=f"whatsapp:+5511999999999",
+  media_url="https://github.com/hugomos.png"
 )
 
 print("Message SID: " + message.sid)
