8c668f8669dece4b006efb1b39bbf8135e616fb5
Vitor Hugo Oliveira <vitor_osantos@hotmail.com>
2024-05-23 09:53:31 -0300

    feat: add twilio phone number variable to .env

--- src/main/config/env.py
+++ src/main/config/env.py
@@ -7,6 +7,7 @@ def envs() -> dict:
   envs = {
     "TWILIO_ACCOUNT_SID": os.getenv("TWILIO_ACCOUNT_SID"),
     "TWILIO_AUTH_TOKEN": os.getenv("TWILIO_AUTH_TOKEN"),
+    "TWILIO_FROM_NUMBER": os.getenv("TWILIO_FROM_NUMBER"),
   }
 
   for k, v in envs.items():
